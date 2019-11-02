cafa4735eba16f5bd218b0214c4f3612d46c1762
Patrick Bollmann <39247927+patrickbollmann@users.noreply.github.com>
2019-11-02 12:42:42 +0100

    added eim.upb.de, math.upb.de

--- upbTerminCrawler.py
+++ upbTerminCrawler.py
@@ -91,3 +91,41 @@ for e in event:
             print(e)
     except(IndexError):
         pass
+
+
+#eim.upb.de
+
+r = requests.get('https://www.eim.uni-paderborn.de/eim-termine-list/')
+r.headers['content-type']
+content =r.text
+event = content.split("<!-- THIS SECTION IS A SINGLE EVENT -->")
+event.pop(0)
+for e in event:
+    try:
+        w = e.split("<div class=\"date\">")
+        x = w[1].split(", ")
+        d = x[1].split("| ")
+        date = d[0].strip()
+        t = d[1].split("</div>")
+        time = t[0].split(" ")[0].strip()
+        l = e.split("<a href=\"")
+        li = l[1].split("/\">")
+        link = li[0].strip()
+        ti = li[1].split("</a>")
+        title = ti[0].strip()
+        te = e.split("<span><p>")
+        te1 = te[1].split("</p></span>")[0]
+        teaser = te1.strip().replace("<em>","").replace("</em>","").replace("&nbsp;","")
+        location = getloc(link)
+
+        time = datetime.strptime(time,"%H.%M").strftime("%H:%M")
+        date = datetime.strptime(date,"%d.%m.%Y").strftime("%Y-%m-%d")
+        row=[date, time, title, teaser, link, location, "27"]
+        try:
+            cur.execute("INSERT INTO `event` (`date`, `time`, `name`, `description`, `link`, `location`, `person_id`) VALUES ("+",".join(list(map(lambda x:"\""+x+"\"",row)))+ ");")
+            conn.commit()
+            print("Termin: "+title+" Eingetragen")
+        except pymysql.IntegrityError as e:
+            print(e)
+    except(IndexError):
+        pass
